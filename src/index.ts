f3558cec7912fca21207d0853ebaeda7c5e64496
song_woojin <ujins8201@gmail.com>
2025-10-01 11:55:28 +0900

    feat: add transfer to Address function

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
 import 'dotenv/config';
 import { ethers } from 'ethers';
 
-const INFURA_API_KEY = process.env.INFURA_API_KEY!;
-const PRIVATE_KEY     = process.env.WALLET_PRIVATE_KEY!;
+const infuraApiKey = process.env.INFURA_API_KEY!;
+const privateKey = process.env.WALLET_PRIVATE_KEY!;
+const toAddress = process.env.TO_ADDRESS!; // ujins 지갑 주소.
 
-const provider = new ethers.InfuraProvider('sepolia', INFURA_API_KEY);
-const wallet = new ethers.Wallet(PRIVATE_KEY, provider);
+const provider = new ethers.InfuraProvider('sepolia', infuraApiKey); //
+const wallet = new ethers.Wallet(privateKey, provider); // 요개 블로그 기준 signer
 
 async function main() {
     console.log('Wallet Address:', wallet.address);
@@ -13,5 +14,21 @@ async function main() {
     const block = await provider.getBlockNumber()
     console.log('have eth:', ethers.formatEther(bal), 'ETH');
     console.log('block_num', ethers.formatEther(block))
+
+    const tx = {
+        to: toAddress,
+        value: ethers.parseEther('0.004'),
+    }
+
+    console.log('sending tx...');
+    const sent = await wallet.sendTransaction(tx);
+    console.log('tx hash:', sent.hash);
+
+    const receipt = await sent.wait(); // 1~2 블록 대기
+    console.log("confirmed in block:", receipt?.blockNumber);
+
+    const balReceiver = await provider.getBalance(toAddress);
+    console.log("after address balance:", ethers.formatEther(balReceiver), "ETH");
 }
+
 main().catch(console.error);
